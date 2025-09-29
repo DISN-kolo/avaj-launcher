d05806d3e8bd92d45c7c7a6f9d06d2284bdbc735
DISN-kolo <arrtemsky@gmail.com>
2025-09-29 17:43:50 +0200

    basic steps parsing with scanner. added test scenarios for that

--- src/ft/avaj/simulator/Simulator.java
+++ src/ft/avaj/simulator/Simulator.java
@@ -5,6 +5,7 @@ import	java.io.FileNotFoundException;
 import	java.util.Scanner;
 
 public class Simulator {
+	private static int	stepsAmt = 0;
 	public static void main(String[] args) {
 		if (args.length != 1) {
 			System.err.println("Usage: <program_name> <scenario_filename>");
@@ -14,7 +15,27 @@ public class Simulator {
 		try {
 			Scanner sc = new Scanner(new File(args[0]));
 			System.out.println("Created a sc");
+			boolean	isFirstLine = true;
 			while (sc.hasNextLine()) {
+				if (isFirstLine) {
+					if (sc.hasNextInt()) {
+						Simulator.stepsAmt = sc.nextInt();
+						if (stepsAmt <= 0) {
+							System.err.println("Incorrect amount of steps (must be > 0)");
+							return ;
+						}
+						System.out.println("Parsed steps amount: " + Simulator.stepsAmt);
+						String remainder = sc.nextLine();
+						System.out.println("The remainder is: '" + remainder + "'");
+					}
+					else
+					{
+						System.err.println("Steps weren't specified");
+						return ;
+					}
+					isFirstLine = false;
+					continue ;
+				}
 				System.out.println(sc.nextLine());
 			}
 		}
