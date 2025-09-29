ec20989b4827e5faf249de96e477bd39d12fdc3b
DISN-kolo <arrtemsky@gmail.com>
2025-09-29 16:47:03 +0200

    scan a file and go thru it line by line

--- src/ft/avaj/simulator/Simulator.java
+++ src/ft/avaj/simulator/Simulator.java
@@ -1,5 +1,9 @@
 package ft.avaj.simulator;
 
+import	java.io.File;
+import	java.io.FileNotFoundException;
+import	java.util.Scanner;
+
 public class Simulator {
 	public static void main(String[] args) {
 		if (args.length != 1) {
@@ -7,5 +11,15 @@ public class Simulator {
 			return ;
 		}
 		System.out.println("Processing " + args[0]);
+		try {
+			Scanner sc = new Scanner(new File(args[0]));
+			System.out.println("Created a sc");
+			while (sc.hasNextLine()) {
+				System.out.println(sc.nextLine());
+			}
+		}
+		catch (Exception e) {
+			e.printStackTrace();
+		}
 	}
 }
